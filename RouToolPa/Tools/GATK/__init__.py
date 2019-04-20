29c9818b421d02aec7755ff96a60c3368df4e065
mahajrod <mahajrod@gmail.com>
2019-04-20 19:00:12 +0700

    additions

--- RouToolPa/Tools/GATK/__init__.py
+++ RouToolPa/Tools/GATK/__init__.py
@@ -1,21 +1,21 @@
 __author__ = 'mahajrod'
 
-from RouToolPa.Tools.GATK import PrintReads
-from RouToolPa.Tools.GATK import CatVariants
-from RouToolPa.Tools.GATK import GenotypeGVCFs
-from RouToolPa.Tools.GATK import IndelRealigner
-from RouToolPa.Tools.GATK import SelectVariants
-from RouToolPa.Tools.GATK import CombineVariants
-from RouToolPa.Tools.GATK import HaplotypeCaller
-from RouToolPa.Tools.GATK import ValidateVariants
-from RouToolPa.Tools.GATK import BaseRecalibrator
-from RouToolPa.Tools.GATK import UnifiedGenotyper
-from RouToolPa.Tools.GATK import AnalyzeCovariates
-from RouToolPa.Tools.GATK import VariantFiltration
-from RouToolPa.Tools.GATK import ApplyRecalibration
-from RouToolPa.Tools.GATK import VariantRecalibrator
-from RouToolPa.Tools.GATK import RealignerTargetCreator
-from RouToolPa.Tools.GATK import FastaAlternateReferenceMaker
+from RouToolPa.Tools.GATK.PrintReads import PrintReads
+from RouToolPa.Tools.GATK.CatVariants import CatVariants
+from RouToolPa.Tools.GATK.GenotypeGVCFs import GenotypeGVCFs
+from RouToolPa.Tools.GATK.IndelRealigner import IndelRealigner
+from RouToolPa.Tools.GATK.SelectVariants import SelectVariants
+from RouToolPa.Tools.GATK.CombineVariants import CombineVariants
+from RouToolPa.Tools.GATK.HaplotypeCaller import HaplotypeCaller
+from RouToolPa.Tools.GATK.ValidateVariants import ValidateVariants
+from RouToolPa.Tools.GATK.BaseRecalibrator import BaseRecalibrator
+from RouToolPa.Tools.GATK.UnifiedGenotyper import UnifiedGenotyper
+from RouToolPa.Tools.GATK.AnalyzeCovariates import AnalyzeCovariates
+from RouToolPa.Tools.GATK.VariantFiltration import VariantFiltration
+from RouToolPa.Tools.GATK.ApplyRecalibration import ApplyRecalibration
+from RouToolPa.Tools.GATK.VariantRecalibrator import VariantRecalibrator
+from RouToolPa.Tools.GATK.RealignerTargetCreator import RealignerTargetCreator
+from RouToolPa.Tools.GATK.FastaAlternateReferenceMaker import FastaAlternateReferenceMaker
 
 
 PrintReads = PrintReads()
