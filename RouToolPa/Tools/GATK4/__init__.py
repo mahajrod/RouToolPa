72854d5d5f5a5b40b29bc7519350c97d8ef69342
mahajrod <mahajrod@gmail.com>
2019-06-26 11:56:36 +0300

    fixes

--- RouToolPa/Tools/GATK4/__init__.py
+++ RouToolPa/Tools/GATK4/__init__.py
@@ -2,9 +2,11 @@ __author__ = 'mahajrod'
 
 from RouToolPa.Tools.GATK4.SortVcf4 import SortVcf4
 from RouToolPa.Tools.GATK4.GenotypeGVCFs4 import GenotypeGVCFs4
+from RouToolPa.Tools.GATK4.GenomicsDBImport4 import GenomicsDBImport4
 #from RouToolPa.Tools.GATK4.SelectVariants4 import SelectVariants4
 #from RouToolPa.Tools.GATK4.CombineVariants4 import CombineVariants4
 from RouToolPa.Tools.GATK4.HaplotypeCaller4 import HaplotypeCaller4
+from RouToolPa.Tools.GATK4.GenomicsDBImport4 import GenomicsDBImport4
 from RouToolPa.Tools.GATK4.ValidateVariants4 import ValidateVariants4
 from RouToolPa.Tools.GATK4.BaseRecalibrator4 import BaseRecalibrator4
 #from RouToolPa.Tools.GATK4.UnifiedGenotyper4 import UnifiedGenotyper4
@@ -17,6 +19,7 @@ GenotypeGVCFs4 = GenotypeGVCFs4()
 #SelectVariants4 = SelectVariants4()
 #CombineVariants4 = CombineVariants4()
 HaplotypeCaller4 = HaplotypeCaller4()
+GenomicsDBImport4 = GenomicsDBImport4()
 ValidateVariants4 = ValidateVariants4()
 #BaseRecalibrator4 = BaseRecalibrator4()
 #UnifiedGenotyper4 = UnifiedGenotyper4()
