43dace02a77f7eddcd6e6f9c4ec8996dc1595504
mahajrod <mahajrod@gmail.com>
2019-07-02 23:37:31 +0300

    EMPHF added

--- RouToolPa/Tools/Kmers/Jellyfish.py
+++ RouToolPa/Tools/Kmers/Jellyfish.py
@@ -37,9 +37,32 @@ class Jellyfish(Tool):
         options += " -C" if count_both_strands else ""
         options += " -L %i" % lower_count if lower_count is not None else ""
         options += " -U %i" % upper_count if upper_count is not None else ""
-        options += " %s" % (" ".join(in_file) if (isinstance(in_file, list)) or (isinstance(in_file, tuple)) else in_file)
 
-        self.execute(options, cmd="jellyfish count")
+        input_files = [in_file] if isinstance(in_file, str) else in_file
+        filetypes_list = []
+        for filename in input_files:
+            filetype = self.detect_filetype_by_extension(filename)
+            if filetype not in filetypes_list:
+                filetypes_list.append(filetype)
+
+        if len(filetypes_list) > 1:
+            raise ValueError("Mix of filetypes in input files: %s" % ",".join(filetypes_list))
+
+        if filetypes_list[0] == "fasta" or filetypes_list[0] == "fastq" or filetypes_list[0] is None:
+            if filetypes_list[0] is None:
+                print("Warning!!! Type of input files was not recognized. Treating them as fasta...")
+            cmd = "jellyfish count"
+            options += " %s" % " ".join(input_files)
+        elif filetypes_list[0] == "bzip":
+            file_options = "bzcat %s" % " ".join(input_files)
+            cmd = "%s | jellyfish count" % file_options
+            options += " /dev/fd/0"
+        elif filetypes_list[0] == "gz":
+            file_options = "zcat %s" % " ".join(input_files)
+            cmd = "%s | jellyfish count" % file_options
+            options += " /dev/fd/0"
+
+        self.execute(options, cmd=cmd)
 
     def stats(self, in_file, out_file, lower_count=None, upper_count=None):
 
@@ -87,17 +110,29 @@ class Jellyfish(Tool):
 
         self.execute(options, cmd="jellyfish dump")
 
+    def dump_kmers(self, in_file, output_prefix, lower_count=None, upper_count=None):
+
+        if (lower_count is not None) and (upper_count is not None):
+            if lower_count > upper_count:
+                raise ValueError("Upper limit for kmer counts is less than lower")
+
+        options = " -t"     # use tab separator
+        options += " -L %i" % lower_count if lower_count is not None else ""
+        options += " -U %i" % upper_count if upper_count is not None else ""
+        options += " -c"    # column_format
+        options += " %s" % in_file
+        options += " | tee %s.counts | cut -f 1 " % output_prefix
+        options = " > %s.kmers" % output_prefix
+
+        self.execute(options, cmd="jellyfish dump")
+
     def get_kmer_list(self, in_file, out_prefix, kmer_length=23, hash_size=1000000, count_both_strands=False,
                       lower_count=None, upper_count=None):
-        base_file = "%s_%i_mer.jf" % (out_prefix, kmer_length)
-        kmer_table_file = "%s_%i_mer.counts" % (out_prefix, kmer_length)
-        kmer_file = "%s_%i_mer.kmer" % (out_prefix, kmer_length)
+        base_file = "%s.%i.jf" % (out_prefix, kmer_length)
         self.count(in_file, base_file, kmer_length=kmer_length, hash_size=hash_size,
                    count_both_strands=count_both_strands,
                    lower_count=lower_count, upper_count=upper_count)
-        self.dump(base_file, kmer_table_file)
-        sed_string = 'sed -e "s/\t.*//" %s > %s' % (kmer_table_file, kmer_file)
-        os.system(sed_string)
+        self.dump_kmers(in_file, out_prefix, lower_count=lower_count, upper_count=upper_count)
 
     def draw_kmer_distribution(self, histo_file, kmer_length, output_prefix, output_formats=["svg", "png"],
                                logbase=10, non_log_low_limit=5, non_log_high_limit=100, order=3, mode="wrap",
