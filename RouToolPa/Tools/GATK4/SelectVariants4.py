30586e68a52f0bcec1140f6ef06cda22eb2656f9
mahajrod <mahajrod@gmail.com>
2019-08-17 18:48:40 +0700

    VCF parser fixes

--- RouToolPa/Tools/GATK4/SelectVariants4.py
+++ RouToolPa/Tools/GATK4/SelectVariants4.py
@@ -4,10 +4,10 @@ from RouToolPa.Tools.Abstract import Tool
 
 class SelectVariants4(Tool):
 
-    def __init__(self,  java_path="", max_threads=4, jar_path="", max_memory=None, timelog=None):
-        Tool.__init__(self, "gatk SelectVariants", java_path=java_path,
-                          max_threads=max_threads, jar_path=jar_path, max_memory=max_memory,
-                          timelog=timelog)
+    def __init__(self,  path="", max_threads=4,  max_memory=None, timelog=None):
+        Tool.__init__(self, "gatk SelectVariants", path=path,
+                      max_threads=max_threads,max_memory=max_memory,
+                      timelog=timelog)
     # http://www.broadinstitute.org/gatk/gatkdocs/org_broadinstitute_sting_gatk_walkers_variantutils_SelectVariants.html
     # selectType
     # INDEL
