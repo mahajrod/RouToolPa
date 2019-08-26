c1665077175bd0a546b8bcf9445a36ac9e07ebe3
mahajrod <mahajrod@gmail.com>
2019-08-26 22:39:52 +0700

    VCF parser fixes

--- RouToolPa/Routines/__init__.py
+++ RouToolPa/Routines/__init__.py
@@ -2,11 +2,11 @@ __author__ = 'mahajrod'
 
 from RouToolPa.Routines.GO import GORoutines
 from RouToolPa.Routines.VCF import VCFRoutines
-#from RouToolPa.Routines.Math import MathRoutines
+from RouToolPa.Routines.Math import MathRoutines
 from RouToolPa.Routines.File import FileRoutines
 from RouToolPa.Routines.Tree import TreeRoutines
 from RouToolPa.Routines.NCBI import NCBIRoutines
-#from RouToolPa.Routines.Math import SmoothRoutines
+from RouToolPa.Routines.Math import SmoothRoutines
 from RouToolPa.Routines.MtDNA import MtDNARoutines
 from RouToolPa.Routines.Fastq import FastQRoutines
 from RouToolPa.Routines.Primer import PrimerRoutines
@@ -14,12 +14,12 @@ from RouToolPa.Routines.EggNOG import EggNOGRoutines
 from RouToolPa.Routines.Project import ProjectRoutines
 from RouToolPa.Routines.Ensembl import EnsemblRoutines
 from RouToolPa.Routines.TreeFam import TreeFamRoutines
-#from RouToolPa.Routines.Drawing import DrawingRoutines
+from RouToolPa.Routines.Drawing import DrawingRoutines
 from RouToolPa.Routines.Sequence import SequenceRoutines
 from RouToolPa.Routines.Evolution import EvolutionRoutines
 from RouToolPa.Routines.Alignment import AlignmentRoutines
 from RouToolPa.Routines.Expression import ExpressionRoutines
-#from RouToolPa.Routines.Matplotlib import MatplotlibRoutines
+from RouToolPa.Routines.Matplotlib import MatplotlibRoutines
 from RouToolPa.Routines.Annotations import AnnotationsRoutines
 from RouToolPa.Routines.Phylogenetics import PhylogeneticsRoutines
 from RouToolPa.Routines.SequenceCluster import SequenceClusterRoutines
@@ -28,24 +28,24 @@ from RouToolPa.Routines.MultipleAlignment import MultipleAlignmentRoutines
 
 GORoutines = GORoutines()
 VCFRoutines = VCFRoutines()
-#MathRoutines = MathRoutines()
+MathRoutines = MathRoutines()
 FileRoutines = FileRoutines()
 TreeRoutines = TreeRoutines()
 NCBIRoutines = NCBIRoutines()
 MtDNARoutines = MtDNARoutines()
 FastQRoutines = FastQRoutines()
-#SmoothRoutines = SmoothRoutines()
+SmoothRoutines = SmoothRoutines()
 PrimerRoutines = PrimerRoutines()
 EggNOGRoutines = EggNOGRoutines()
 ProjectRoutines = ProjectRoutines()
 EnsemblRoutines = EnsemblRoutines()
 TreeFamRoutines = TreeFamRoutines()
-#DrawingRoutines = DrawingRoutines()
+DrawingRoutines = DrawingRoutines()
 SequenceRoutines = SequenceRoutines()
 EvolutionRoutines = EvolutionRoutines()
 AlignmentRoutines = AlignmentRoutines()
 ExpressionRoutines = ExpressionRoutines()
-#MatplotlibRoutines = MatplotlibRoutines()
+MatplotlibRoutines = MatplotlibRoutines()
 AnnotationsRoutines = AnnotationsRoutines()
 PhylogeneticsRoutines = PhylogeneticsRoutines()
 SequenceClusterRoutines = SequenceClusterRoutines()
