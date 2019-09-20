871a5dd45d7fce98a281148a043378a45b4f3b23
mahajrod <mahajrod@gmail.com>
2019-09-20 18:39:49 +0700

    VCF parser fixes

--- RouToolPa/Tools/WGA/LAST.py
+++ RouToolPa/Tools/WGA/LAST.py
@@ -50,7 +50,8 @@ class LAST(Tool):
 
     def parse_lastal_options(self, lastdb, query, output, verbose=True,
                              keep_preliminary_masking=True, mask_simple_repeats=True,
-                             output_format="MAF", per_thread_memory="4G"):
+                             output_format="MAF", per_thread_memory="4G", match_score_matrix=None,
+                             eg2_threshold=0.05, discard_limit=2):
 
         options = " -P %i" % self.threads
 
@@ -59,6 +60,10 @@ class LAST(Tool):
                                 1 if mask_simple_repeats else 0)
         options += " -f %s" % output_format if output_format else ""
         options += " -i %s" % per_thread_memory if per_thread_memory else ""
+        options += " -p %s" % match_score_matrix
+
+        options += " -E %f" % eg2_threshold if eg2_threshold else ""
+        options += " -C %i" % discard_limit if discard_limit else ""
 
         options += " %s" % lastdb
         options += " %s" % query
@@ -71,15 +76,37 @@ class LAST(Tool):
             options += " > %s" % output
         return options
 
+    def parse_last_train_options(self, lastdb, query, output, verbose=True,
+                                 reverse_complement_symmetry=True, directional_symmetry=True,
+                                 insertion_deletion_symmetry=True, eg2_threshold=0.05,
+                                 discard_limit=2):
+
+        options = " -P %i" % self.threads
+
+        options += " -v" if verbose else ""
+        options += " --revsym" if reverse_complement_symmetry else ""
+        options += " --matsym" if directional_symmetry else ""
+        options += " --gapsym" if insertion_deletion_symmetry else ""
+        options += " -E %f" % eg2_threshold if eg2_threshold else ""
+        options += " -C %i" % discard_limit if discard_limit else ""
+
+        options += " %s" % lastdb
+        options += " %s" % query
+        options += " > %s" % output
+
+        return options
+
     def lastal(self, lastdb, query, output, verbose=True,
                keep_preliminary_masking=True, mask_simple_repeats=True,
-               output_format="MAF", per_thread_memory="4G"):
+               output_format="MAF", per_thread_memory="4G", match_score_matrix=None,
+               eg2_threshold=0.05, discard_limit=2):
 
         options = self.parse_lastal_options(lastdb, query, output, verbose=verbose,
                                             keep_preliminary_masking=keep_preliminary_masking,
                                             mask_simple_repeats=mask_simple_repeats,
                                             output_format=output_format,
-                                            per_thread_memory=per_thread_memory)
+                                            per_thread_memory=per_thread_memory, match_score_matrix=match_score_matrix,
+                                            eg2_threshold=eg2_threshold, discard_limit=discard_limit)
 
         self.execute(options=options, cmd="lastal")
 
@@ -90,6 +117,33 @@ class LAST(Tool):
         options += " > %s" % output_file
 
         self.execute(options=options, cmd="maf-convert")
+
+    def train(self, lastdb, query, output, verbose=True,
+              reverse_complement_symmetry=True, directional_symmetry=True,
+              insertion_deletion_symmetry=True, eg2_threshold=0.05,
+              discard_limit=2):
+
+        options = self.parse_last_train_options(lastdb, query, output, verbose=verbose,
+                                                reverse_complement_symmetry=reverse_complement_symmetry,
+                                                directional_symmetry=directional_symmetry,
+                                                insertion_deletion_symmetry=insertion_deletion_symmetry,
+                                                eg2_threshold=eg2_threshold,
+                                                discard_limit=discard_limit)
+
+        self.execute(options=options, cmd="last-train")
+
+    def extract_one_to_one_alignments(self, input_maf, output_prefix, max_mismap_probability=1):
+        options = ""
+        options += " -m %f" % max_mismap_probability if max_mismap_probability else ""
+        options += " %s" % input_maf
+        options += " | tee %s.first_split.maf" % output_prefix
+
+        options += " | maf-swap | last-split"
+        options += " -m %f" % max_mismap_probability if max_mismap_probability else ""
+        options += " | maf-swap"
+        options += " > %s.one_to_one.maf" % output_prefix
+
+        self.execute(options, cmd="last-split")
 
     def filter_tab_output_by_length(self, input_tab, min_hit_len, output_tab, mode="target", verbose=False):
 
