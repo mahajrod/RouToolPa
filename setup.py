d26d82f14db000be7f415705dbbb600383ebf374
mahajrod <mahajrod@gmail.com>
2019-11-08 18:47:39 +0300

    VCF parser fixes

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ __author__ = 'mahajrod'
 import os
 from setuptools import setup, find_packages
 
+dependencies = ['scipy', 'numpy', 'pandas', 'matplotlib', 'matplotlib-venn',
+                'biopython', 'xmltodict', 'bcbio-gff', 'statsmodels']
+
 setup(name='MAVR',
-      version='0.58',
+      version='0.80',
       packages=find_packages(),
       author='Sergei F. Kliver',
       author_email='mahajrod@gmail.com',
-      install_requires=['scipy', 'numpy', 'pandas', 'matplotlib', 'matplotlib-venn', 'ete2', 'biopython', 'xmltodict', 'bcbio-gff', 'statsmodels'],
+      install_requires=dependencies,
       long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),)
