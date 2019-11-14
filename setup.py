e0267bdf42a44c8b94e377a60ace10f3c1017ed2
mahajrod <mahajrod@gmail.com>
2019-11-14 13:42:14 +0300

    VCF parser fixes

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 __author__ = 'mahajrod'
+
 import os
+import sys
 from setuptools import setup, find_packages
 
 dependencies = ['scipy', 'numpy', 'pandas', 'matplotlib', 'matplotlib-venn',
                 'biopython', 'xmltodict', 'bcbio-gff', 'statsmodels']
+if sys.version_info[0] == 3:
+    dependencies += ["ete3"]
+elif sys.version_info[0] == 2:
+    dependencies += ["ete2"]
+else:
+    raise ValueError("ERROR!!! Unsupported python version: %s" % str(sys.version_info[0]))
 
-setup(name='MAVR',
-      version='0.80',
+setup(name='RouToolPa',
+      version='0.83',
       packages=find_packages(),
       author='Sergei F. Kliver',
       author_email='mahajrod@gmail.com',
