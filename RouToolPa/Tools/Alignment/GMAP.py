562a7e0394357e7e01a30129030cf2a5622c4d1a
mahajrod <mahajrod@gmail.com>
2019-12-05 06:09:36 +0700

    fixes

--- RouToolPa/Tools/Alignment/GMAP.py
+++ RouToolPa/Tools/Alignment/GMAP.py
@@ -13,7 +13,7 @@ class GMAP(Tool):
         pass
 
     def filter_gff(self, in_file, out_file, min_coverage=None, min_identity=None, gene_feature="gene",
-                   transcript_feature="mRNA", exon_feature="exon", cds_feature="CDS"):
+                   transcript_feature="mRNA", exon_feature="exon", cds_feature="CDS", verbose=False):
         if min_coverage is not None and min_identity is not None:
             def check(attributes_dict):
                 if (float(attributes_dict["coverage"]) >= min_coverage) and (float(attributes_dict["identity"]) >= min_identity):
@@ -33,12 +33,16 @@ class GMAP(Tool):
             raise ValueError("ERROR!!! Neither minimum coverage nor minimum identity was set!")
 
         line_list_gen = self.file_line_as_list_generator(in_file)
+        total_transcripts = 0
+        retained_transcripts = 0
         with open(out_file, "w") as out_fd:
             write_flag = False
             for line_list in line_list_gen:
                 if line_list[2] == transcript_feature:
+                    total_transcripts += 1
                     attributtes_dict = dict(map(lambda s: s.split("="), line_list[8].split(";")))
                     if check(attributtes_dict):
+                        retained_transcripts += 1
                         out_fd.write("\t".join(line_list))
                         out_fd.write("\n")
                         write_flag = True
@@ -48,3 +52,6 @@ class GMAP(Tool):
                 elif ((line_list[2] == cds_feature) or (line_list[2] == exon_feature)) and write_flag:
                     out_fd.write("\t".join(line_list))
                     out_fd.write("\n")
+
+        if verbose:
+            print("Total transcripts: %i\n Retained transcripts: %i\n" % (total_transcripts, retained_transcripts))
