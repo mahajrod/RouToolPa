ea4f7d103cf7992d32dae589d8820d7df4e9f871
mahajrod <mahajrod@gmail.com>
2019-12-05 06:02:54 +0700

    fixes

--- RouToolPa/Routines/File.py
+++ RouToolPa/Routines/File.py
@@ -628,10 +628,7 @@ class FileRoutines:
             for line in in_fd:
                 if line[:comments_prefix_len] == comments_prefix:
                     continue
-                try:
-                    yield line.strip().split(separator)
-                except StopIteration:
-                    break
+                yield line.strip().split(separator)
 
     def file_line_generator(self, input_file, comments_prefix="#"):
         comments_prefix_len = len(comments_prefix)
