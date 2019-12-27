5bb0e3166d3b08a303add9a5816d246300a8d8e2
mahajrod <mahajrod@gmail.com>
2019-12-28 02:20:12 +0700

    fixes

---
+++ RouToolPa/Tools/NCBIToolkit/FastqDump.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+from RouToolPa.Tools.Abstract import Tool
+from RouToolPa.Routines import FileRoutines
+
+
+class FastqDump(Tool):
+    def __init__(self, path="", max_threads=4):
+        Tool.__init__(self, "fastq-dump", path=path, max_threads=max_threads)
+
+    def parse_options(self, split_pe=True, retain_original_ids=True):
+
+        options = ""
+        options += " --split-3" if split_pe else ""
+        options += " --origfmt" if retain_original_ids else ""
+
+        return options
+
+    def parallel_download(self, sra_id_list, output_dir, split_pe=True, retain_original_ids=True):
+
+        common_options = self.parse_options(split_pe=split_pe, retain_original_ids=retain_original_ids)
+
+        sra_id_list = [sra_id_list] if isinstance(sra_id_list, str) else sra_id_list
+
+        option_list = []
+        for sra_id in sra_id_list:
+            sra_id_dir = "%s/%s" % output_dir, sra_id
+            try:
+                os.mkdir(sra_id_dir)
+            except OSError:
+                pass
+
+            option = common_options
+            option += " -O %s" % sra_id_dir
+            option += " %s" % sra_id
+
+            option_list.append(option)
+
+        self.parallel_execute(option_list)
---
+++ RouToolPa/Tools/NCBIToolkit/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'mahajrod'
+
+from RouToolPa.Tools.NCBIToolkit.FastqDump import FastqDump
+
+
+FastqDump = FastqDump()
