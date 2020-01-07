700b3d5bb1fd4b74a938769b48141d06e7fd4772
mahajrod <mahajrod@gmail.com>
2020-01-07 14:38:27 +0700

    fixes

--- RouToolPa/Tools/Alignment/__init__.py
+++ RouToolPa/Tools/Alignment/__init__.py
@@ -1,8 +1,9 @@
 __author__ = 'mahajrod'
 
-from RouToolPa.Tools.Alignment.BLAT import BLAT
+from RouToolPa.Tools.Alignment.EMA import EMA
 from RouToolPa.Tools.Alignment.GEM import GEM
 from RouToolPa.Tools.Alignment.BWA import BWA
+from RouToolPa.Tools.Alignment.BLAT import BLAT
 from RouToolPa.Tools.Alignment.TMAP import TMAP
 from RouToolPa.Tools.Alignment.STAR import STAR
 from RouToolPa.Tools.Alignment.GMAP import GMAP
@@ -11,7 +12,7 @@ from RouToolPa.Tools.Alignment.Bowtie2 import Bowtie2
 from RouToolPa.Tools.Alignment.Novoalign import Novoalign
 from RouToolPa.Tools.Alignment.LongRanger import LongRanger
 
-
+EMA = EMA()
 GEM = GEM()
 BWA = BWA()
 TMAP = TMAP()
