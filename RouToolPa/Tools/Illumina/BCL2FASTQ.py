4c68b6407f4b204c2f22ef3bc745d326a33bd743
mahajrod <mahajrod@gmail.com>
2020-01-22 01:13:17 +0700

    fixes

--- RouToolPa/Tools/Illumina/BCL2FASTQ.py
+++ RouToolPa/Tools/Illumina/BCL2FASTQ.py
@@ -3,37 +3,40 @@ __author__ = 'Sergei F. Kliver'
 import os
 import shutil
 from RouToolPa.Tools.Abstract import Tool
-t
 
 
 class BCL2FASTQ(Tool):
     """
-    http://hmmer.janelia.org/
+    TODO: NOT ALL options were implemented
     """
     def __init__(self, path="", max_threads=4):
         Tool.__init__(self, "bcl2fastq", path=path, max_threads=max_threads)
 
-    def hmmbuild(self, input, output, sample_name, summary_output="hmmbuild_summary.t",
-                 data_type="protein", max_insert_len=None, window_length=None,
-                 alt_model_constact_strategy=None, alt_rel_seq_weigthing_strategy=None,
-                 alt_eff_weighting_strategy=None, alt_prior_strategy=None, ):
+    def parse_options(self, run_dir, output_dir, sample_sheet, intensities_dir=None, barcode_mismatches=None,
+                      loading_threads=None, processing_threads=None, writing_threads=None):
 
-        options = " --cpu %i" % self.threads
-        options += " --dna" if data_type == "dna" else " --rna" if data_type == "rna" else " --amino"
-        options += " -n %s" % sample_name
-        options += " --maxinsertlen %i" % max_insert_len if max_insert_len else ""
-        options += " --w_length %i" % window_length if window_length else ""
-        options += " --%s" % alt_model_constact_strategy if alt_model_constact_strategy else ""
-        options += " --%s" % alt_rel_seq_weigthing_strategy if alt_rel_seq_weigthing_strategy else ""
-        options += " --%s" % alt_eff_weighting_strategy if alt_eff_weighting_strategy else ""
-        options += " --%s" % alt_prior_strategy if alt_prior_strategy else ""
-        options += " -o %s" % summary_output
-        options += " %s" % output
-        options += " %s" % input
+        options = " -r %i" % loading_threads if loading_threads else self.threads
+        options += " -p %i" % processing_threads if processing_threads else self.threads
+        options += " -w %i" % writing_threads if writing_threads else self.threads
 
-        self.execute(options, cmd="hmmbuild")
+        options += "-i %s" % intensities_dir if intensities_dir else ""# Ex 191228_M02435_0069_000000000-CP7BV/Data/Intensities/BaseCalls/
+        options += " --output-dir %s" % output_dir
+        options += " -R %s" % run_dir # Ex 191228_M02435_0069_000000000-CP7BV
+        options += " --sample-sheet %s" % sample_sheet
 
+        options += " --barcode-mismatches %i" % barcode_mismatches if barcode_mismatches else "" #Allowed: 0,1(default),2
 
+        return options
 
-if __name__ == "__main__":
-    pass
+    def demultiplex(self, run_dir, output_dir, sample_sheet, intensities_dir=None, barcode_mismatches=None,
+                    loading_threads=None, processing_threads=None, writing_threads=None):
+
+        options = self.parse_options(run_dir, output_dir, sample_sheet,
+                                     intensities_dir=intensities_dir,
+                                     barcode_mismatches=barcode_mismatches,
+                                     loading_threads=loading_threads,
+                                     processing_threads=processing_threads,
+                                     writing_threads=writing_threads)
+
+        self.execute(options=options)
+
