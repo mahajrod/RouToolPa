a78f3b26a70efce49e5d64c698cea0848884f660
mahajrod <mahajrod@gmail.com>
2020-01-29 01:21:48 +0700

    fixes

--- RouToolPa/Tools/Samtools/__init__.py
+++ RouToolPa/Tools/Samtools/__init__.py
@@ -1,7 +1,8 @@
 __author__ = 'mahajrod'
 
 from RouToolPa.Tools.Samtools.Samtools import *
-
+from RouToolPa.Tools.Samtools.VariantCall import VariantCall
 
 SamtoolsV1 = SamtoolsV1()
 SamtoolsV0 = SamtoolsV0()
+VariantCall = VariantCall()
