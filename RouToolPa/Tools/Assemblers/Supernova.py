5cb354f56ece5682812a8a96a0f5cc7eb15616e4
mahajrod <zmeenosez72>
2020-04-19 14:15:59 +0300

    fixes

--- RouToolPa/Tools/Assemblers/Supernova.py
+++ RouToolPa/Tools/Assemblers/Supernova.py
@@ -8,3 +8,21 @@ from RouToolPa.Tools.Abstract import Tool
 class Supernova(Tool):
     def __init__(self, path="", max_threads=4):
         Tool.__init__(self, "supernova", path=path, max_threads=max_threads)
+
+    def generate_fasta(self, assembly_dir, output_prefix, minsize=1000, headers="full"):
+
+        options = " mkoutput"
+        options += " --asmdir %s" % assembly_dir
+        options += " --minsize %i" % minsize
+        options += " --headers %s" % headers
+
+        options_list = []
+
+        for out_type in "raw", "megabubbles", "pseudohap", "pseudohap2":
+            tmp_options = options
+            tmp_options += " --style %s" % out_type
+            tmp_options += " --outprefix %s.%s" % (output_prefix, out_type)
+
+            options_list.append(tmp_options)
+
+        self.parallel_execute(options_list=options_list)
