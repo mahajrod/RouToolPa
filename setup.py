218c50b595dc9ad495b7d9898b544664db57643d
mahajrod <mahajrod@gmail.com>
2021-06-06 18:39:24 +0300

    fixes

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-dependencies = ['scipy', 'numpy', 'pandas', 'matplotlib', 'matplotlib-venn',
-                'biopython', 'xmltodict', 'bcbio-gff', 'statsmodels']
-
-#  scipy numpy pandas matplotlib matplotlib-venn biopython xmltodict bcbio-gff statsmodels pyparsing ete3
+dependencies = ['scipy', 'numpy', 'pandas', 'matplotlib', 'venn',
+                'biopython', 'xmltodict', 'statsmodels', "ete3", "bcbio-gff"]
+if sys.version_info[1] < 7:  # check minor version of python3
+    dependencies += ["importlib_resources"]
+"""    
+#  scipy numpy pandas matplotlib  biopython xmltodict bcbio-gff statsmodels pyparsing ete3 venn
 
 if sys.version_info[0] == 3: # major version
     dependencies += ["ete3", 'venn', ]
@@ -19,9 +21,9 @@ elif sys.version_info[0] == 2:
     dependencies += ["importlib_resources"]
 else:
     raise ValueError("ERROR!!! Unsupported python version: %s" % str(sys.version_info[0]))
-
+"""
 setup(name='RouToolPa',
-      version='0.86',
+      version='0.92',
       packages=find_packages(),
       author='Sergei F. Kliver',
       author_email='mahajrod@gmail.com',
