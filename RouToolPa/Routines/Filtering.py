839e591705651deee5c2a7ce5f758c070cb4ad21
mahajrod <mahajrod@gmail.com>
2023-04-26 08:28:46 +0200

    fixes

--- RouToolPa/Routines/Filtering.py
+++ RouToolPa/Routines/Filtering.py
@@ -68,8 +68,5 @@ class FilteringRoutines(SequenceRoutines, FastQRoutines):
              self.metaopen(output, "w", buffering=100000000) as out_fd:
             for kraken_line in kraken_input_fd:
                 kraken_line_list = kraken_line.split()
-                print(kraken_line_list)
-                print(kraken_line_list[2])
-                print(kraken_line_list[2] in taxon_id_list)
                 if kraken_line_list[2] in taxon_id_list:
                     out_fd.write(kraken_line_list[1] + "\n")
